9b39fe28ffa0a0b8f87e8d09a90ccd4496688c2e
a96764 <a96764@aup.edu>
2019-11-12 21:34:42 +0000

    About

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <h1>ABOUT US</h1>
-    <iframe
-      width="960"
-      height="515"
-      src="https://www.youtube.com/embed/7eNibMDX9v0"
-      frameborder="0"
-      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-      allowfullscreen
-    />
+    <p>“Most who come to Aro Ha aren’t looking for just one aspect of health; they’re looking for an adventure that addresses the neglected parts. Maybe it’s physical, maybe it’s spiritual, maybe it’s just a nature deficit. It’s hard to know what we are missing until we’ve found it.”
+
+– Lupita Salinas.</p>
+
+<p>
+Located in the Alps, Onga is designed for the rejuvenation of the human spirit. Adhering to a vision of zen inspired luxury, Aro Ha teams intelligent design with exceptional programming to create an unparalleled wellbeing experience.
+
+The accommodation features a range of single and shared eco suites boasting minimalist luxury design. While guests can retreat to their rooms for moments of solitude, each suite is home to a shared foyer with space to sit and share tea. Every space within Aro Ha embraces its majestic surroundings – from the contrast hydrotherapy spas set into the mountainous earth to the yoga studio with panoramic views of Lake Wakatipu.
+
+Aro Ha is proud to be a self-sustaining retreat that continuously celebrates and draws from it’s a unique and inspiring location. We’ve merged ancient traditions with modern technology to become an industry-leading example of earth-friendly design. From our organic produce grown onsite and stored in ambient, earth-cooled cellars, to our state-of-the-art solar and hydro energy system, each wellness journey leaves no trace. </p>
   </div>
 </template>
