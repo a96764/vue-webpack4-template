6019a9da6f96c016251ec289ff1e6342ccc1eb0c
a96764 <a96764@aup.edu>
2019-11-13 15:13:53 +0000

    Center

--- src/components/Home/WelcomeMessage.vue
+++ src/components/Home/WelcomeMessage.vue
@@ -1,15 +1,54 @@
+
 <template>
   <div>
-    <h3>Hello {{ name }} from my Vue.js page, built with Webpack 4!</h3>
+    <b-carousel
+      id="carousel-1"
+      v-model="slide"
+      :interval="4000"
+      controls
+      indicators
+      background="#ababab"
+      img-width="1024"
+      img-height="480"
+      style="text-shadow: 1px 1px 2px #333;"
+      @sliding-start="onSlideStart"
+      @sliding-end="onSlideEnd"
+    >
+      <b-carousel-slide img-src="https://images.unsplash.com/photo-1534796636912-3b95b3ab5986?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80">
+        <b-container>
+          <b-row>
+            <b-col>
+              <h1>Onga Wellness Resort</h1>
+            </b-col>
+          </b-row>
+        </b-container>
+      </b-carousel-slide>
+
+      <b-carousel-slide img-src="https://i1.wp.com/activenorcal.com/wp-content/uploads/2018/05/L2.jpg?fit=1500%2C843&ssl=1" />
+
+      <b-carousel-slide img-src="https://i.ytimg.com/vi/KVx2Ag_Y4T4/maxresdefault.jpg" />
+
+      <b-carousel-slide img-src="https://mustseeplaces.eu/wp-content/uploads/2017/11/Aro-Ha.jpg" />
+
+      <b-carousel-slide img-src="https://indyeastend.com/wp-content/uploads/2018/08/Trapeze-1.jpg" />
+    </b-carousel>
   </div>
 </template>
 
 <script>
 export default {
-  props: {
-    name: {
-      type: String,
-      required: true
+  data () {
+    return {
+      slide: 0,
+      sliding: null
+    }
+  },
+  methods: {
+    onSlideStart (slide) {
+      this.sliding = true
+    },
+    onSlideEnd (slide) {
+      this.sliding = false
     }
   }
 }
--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <p>“Most who come to Aro Ha aren’t looking for just one aspect of health; they’re looking for an adventure that addresses the neglected parts. Maybe it’s physical, maybe it’s spiritual, maybe it’s just a nature deficit. It’s hard to know what we are missing until we’ve found it.”
+    <p>
+      “Most who come to Aro Ha aren’t looking for just one aspect of health; they’re looking for an adventure that addresses the neglected parts. Maybe it’s physical, maybe it’s spiritual, maybe it’s just a nature deficit. It’s hard to know what we are missing until we’ve found it.”
 
-– Lupita Salinas.</p>
+      – Lupita Salinas.
+    </p>
 
-<p>
-Located in the Alps, Onga is designed for the rejuvenation of the human spirit. Adhering to a vision of zen inspired luxury, Aro Ha teams intelligent design with exceptional programming to create an unparalleled wellbeing experience.
+    <p>
+      Located in the Alps, Onga is designed for the rejuvenation of the human spirit. Adhering to a vision of zen inspired luxury, Aro Ha teams intelligent design with exceptional programming to create an unparalleled wellbeing experience.
 
-The accommodation features a range of single and shared eco suites boasting minimalist luxury design. While guests can retreat to their rooms for moments of solitude, each suite is home to a shared foyer with space to sit and share tea. Every space within Aro Ha embraces its majestic surroundings – from the contrast hydrotherapy spas set into the mountainous earth to the yoga studio with panoramic views of Lake Wakatipu.
+      The accommodation features a range of single and shared eco suites boasting minimalist luxury design. While guests can retreat to their rooms for moments of solitude, each suite is home to a shared foyer with space to sit and share tea. Every space within Aro Ha embraces its majestic surroundings – from the contrast hydrotherapy spas set into the mountainous earth to the yoga studio with panoramic views of Lake Wakatipu.
 
-Aro Ha is proud to be a self-sustaining retreat that continuously celebrates and draws from it’s a unique and inspiring location. We’ve merged ancient traditions with modern technology to become an industry-leading example of earth-friendly design. From our organic produce grown onsite and stored in ambient, earth-cooled cellars, to our state-of-the-art solar and hydro energy system, each wellness journey leaves no trace. </p>
+      Aro Ha is proud to be a self-sustaining retreat that continuously celebrates and draws from it’s a unique and inspiring location. We’ve merged ancient traditions with modern technology to become an industry-leading example of earth-friendly design. From our organic produce grown onsite and stored in ambient, earth-cooled cellars, to our state-of-the-art solar and hydro energy system, each wellness journey leaves no trace.
+    </p>
   </div>
 </template>
